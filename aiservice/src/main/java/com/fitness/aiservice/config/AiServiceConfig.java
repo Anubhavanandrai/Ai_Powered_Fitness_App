c3e59a31fd7c9aae18cf0f99244797337ff4aa34
anubhavanand.rai@gmail.com <anubhavanand.rai@gmail.com>
2026-08-17 19:24:32 +0530

    aiService working is being configured

--- aiservice/src/main/java/com/fitness/aiservice/config/AiServiceConfig.java
+++ aiservice/src/main/java/com/fitness/aiservice/config/AiServiceConfig.java
@@ -1,25 +1,10 @@
 package com.fitness.aiservice.config;
 
-
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 @Configuration
 public class AiServiceConfig {
 
-    @Bean
-    public WebClient.Builder apiBuilder(){
-        return WebClient.builder();
-    }
 
-    @Bean
-    public WebClient aiServiceWebClient(WebClient.Builder webClientBuilder)
-    {
-        return webClientBuilder
-                .baseUrl("https://api.openai.com")
-                .build();
-    }
 }
--- aiservice/src/main/java/com/fitness/aiservice/service/KafkaConsumerService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/KafkaConsumerService.java
@@ -3,19 +3,36 @@ package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.dto.KafkaConsumerData;
 import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+
 @Service
+@AllArgsConstructor
 public class KafkaConsumerService {
-    @PostConstruct
-    public void test() {
-        System.out.println("KafkaConsumerService bean CREATED");
-    }
+
+    private ChatClient chatClient;
+
+
     @KafkaListener(topics = "activity-events", groupId = "ai-service-group")
     public void consume(KafkaConsumerData kafkaConsumerData)
     {      System.out.println("🔥 KAFKA MESSAGE RECEIVED");
-        System.out.println("Received activity from kafka is : " + kafkaConsumerData);
+           System.out.println("Received activity from kafka is : " + kafkaConsumerData);
+
+           try{
+               aiCaller(kafkaConsumerData);
+               System.out.println("Data Sent to OpenAI");
+           }
+           catch(Exception e){
+               throw new RuntimeException("Kafka data not served to OpenAI");
+           }
+    }
+
+    public void aiCaller(KafkaConsumerData consumerDataForOpenai)
+    {
+        return chatClient.prompt(consumerDataForOpenai).call().content();
     }
 
 }
