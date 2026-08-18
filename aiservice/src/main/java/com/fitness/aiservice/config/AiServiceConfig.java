fe984c23ef05d697a3ab407d7a487d4e0ae7bb32
anubhavanand.rai@gmail.com <anubhavanand.rai@gmail.com>
2026-08-18 20:59:39 +0530

    Failed to send data to OpenAi

--- aiservice/src/main/java/com/fitness/aiservice/config/AiServiceConfig.java
+++ aiservice/src/main/java/com/fitness/aiservice/config/AiServiceConfig.java
@@ -1,10 +1,16 @@
 package com.fitness.aiservice.config;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class AiServiceConfig {
 
-
+    @Bean
+    public ChatClient aichatClient(ChatClient.Builder cb)
+    {
+        return cb.build();
+    }
 }
--- aiservice/src/main/java/com/fitness/aiservice/service/KafkaConsumerService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/KafkaConsumerService.java
@@ -1,12 +1,16 @@
 package com.fitness.aiservice.service;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fitness.aiservice.dto.KafkaConsumerData;
-import jakarta.annotation.PostConstruct;
+import com.fitness.aiservice.model.Recommendations;
 import lombok.AllArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+
 
 
 @Service
@@ -14,25 +18,38 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumerService {
 
     private ChatClient chatClient;
+    private ObjectMapper objectMapper;
 
 
     @KafkaListener(topics = "activity-events", groupId = "ai-service-group")
     public void consume(KafkaConsumerData kafkaConsumerData)
-    {      System.out.println("🔥 KAFKA MESSAGE RECEIVED");
+    {
+           System.out.println("🔥 KAFKA MESSAGE RECEIVED");
            System.out.println("Received activity from kafka is : " + kafkaConsumerData);
-
-           try{
-               aiCaller(kafkaConsumerData);
-               System.out.println("Data Sent to OpenAI");
+           try {
+               System.out.println("🔥Data ready  to send");
+               String json = aiCaller(kafkaConsumerData);
+               System.out.println("Data received from aicaller is : "+json);
+               Recommendations  airecommend = objectMapper.readValue(json, Recommendations.class);
+               System.out.println("Response received from AI is  : " + airecommend);
            }
            catch(Exception e){
                throw new RuntimeException("Kafka data not served to OpenAI");
-           }
+            }
     }
 
-    public void aiCaller(KafkaConsumerData consumerDataForOpenai)
-    {
-        return chatClient.prompt(consumerDataForOpenai).call().content();
+    public String aiCaller (KafkaConsumerData consumerDataForOpenai) throws JsonProcessingException {
+
+            String json = objectMapper.writeValueAsString(consumerDataForOpenai);
+        System.out.println("Ready to send prompt ");
+            String prompt = """
+                    Analyze this activity data and genere
+                    ate result contaning activity Type, genereate recommendations , List of improvements and suggesttion and safety measures to be taken.Give response in JSON formate.
+                    Data : %s
+                    """.formatted(json);
+        System.out.println("Ai caller has received prompt: "+prompt);
+            return chatClient.prompt(prompt).call().content();
+
     }
 
 }
