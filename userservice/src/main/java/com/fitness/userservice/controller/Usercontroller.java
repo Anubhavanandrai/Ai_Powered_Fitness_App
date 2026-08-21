c46312df8f2cdfafcf7cf99fe64e0ea7a8d21aee
anubhavanand.rai@gmail.com <anubhavanand.rai@gmail.com>
2026-08-21 18:33:56 +0530

    Spring Securtiy added

--- userservice/src/main/java/com/fitness/userservice/controller/Usercontroller.java
+++ userservice/src/main/java/com/fitness/userservice/controller/Usercontroller.java
@@ -1,8 +1,6 @@
 package com.fitness.userservice.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
