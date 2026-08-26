a5a7b442258df75720574d442a70f835712e7c2f
anubhavanand.rai@gmail.com <anubhavanand.rai@gmail.com>
2026-08-26 21:20:33 +0530

    Password encoder

--- userservice/src/main/java/com/fitness/userservice/config/SecurityConfig.java
+++ userservice/src/main/java/com/fitness/userservice/config/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.fitness.userservice.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
+      @Bean
+       public PasswordEncoder passwordEncoder(String password)
+      {
+          return new BCryptPasswordEncoder();
+      }
 
 }
